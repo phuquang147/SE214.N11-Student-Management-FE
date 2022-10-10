b333a3974f50b8fc2e140ea454d95038c0e8bfca
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-10-10 21:36:40 +0700

    Initial commit

---
+++ src/layouts/MainLayout/NavConfig.jsx
@@ -0,0 +1,35 @@
+// component
+import Iconify from '~/components/Iconify';
+
+const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/',
+    icon: getIcon('eva:pie-chart-2-fill'),
+    children: [
+      {
+        title: 'user',
+        path: '/user',
+      },
+    ],
+  },
+  {
+    title: 'user',
+    path: '/user',
+    icon: getIcon('eva:people-fill'),
+  },
+  {
+    title: 'login',
+    path: '/login',
+    icon: getIcon('eva:lock-fill'),
+  },
+  {
+    title: 'Not found',
+    path: '/404',
+    icon: getIcon('eva:alert-triangle-fill'),
+  },
+];
+
+export default navConfig;
